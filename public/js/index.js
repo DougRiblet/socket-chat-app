e302aeaf27ff1b728b1818dacfb381da514eb285
Doug Riblet <smashzen85@gmail.com>
2017-10-01 16:02:44 -0500

    add jQuery, create form, display incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,14 +11,30 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', function(data){
   console.log('New email: ', data)
+  var li = jQuery('<li></li>');
+  li.text(`${data.from}: ${data.text}`);
+  jQuery('#messagelist').append(li);
 });
 
 // socket.emit with ACKNOWLEDGMENT callback function as 3rd argument
 // 'data' parameter in callback receives info back from server
 
-socket.emit('createMessage', {
-  'from': 'Jerry',
-  'text': 'transitive'
-}, function(data){
-  console.log('Acknowledged!!!!', data);
+// socket.emit('createMessage', {
+//   'from': 'Jerry',
+//   'text': 'transitive'
+// }, function(data){
+//   console.log('Acknowledged!!!!', data);
+// });
+
+jQuery('#message-form').on('submit', function(e){
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message').val()
+  }, function(){
+    console.log('inside jQ message processing');
+  });
 });
+
+
+
