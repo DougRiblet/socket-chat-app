94f7d7aa9f81f464de2260c984f6fc38fddaed0e
Doug Riblet <smashzen85@gmail.com>
2017-10-01 01:37:52 -0500

    socket emit and socket broadcast emit

--- server/server.js
+++ server/server.js
@@ -15,13 +15,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // ## socket.emit will send only to individual user on this connection
+  // ## socket.broadcast.emit will send to all other users
+
+  socket.emit('newMessage', {
+    'from': 'Admin',
+    'text': 'Welcome to chatroom',
+    'createdAt': new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    'from': 'Admin',
+    'text': 'New user joined',
+    'createdAt': new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('create new email: ', message);
+
+    // ## io.emit here will send to all users, including message poster
+
     io.emit('newMessage', {
       'from': message.from,
       'text': message.text,
       'createdAt': new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   'from': message.from,
+    //   'text': message.text,
+    //   'createdAt': new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
