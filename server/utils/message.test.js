49664472a8f4cf6dcd8e959000a9bd34f49a12da
Doug Riblet <smashzen85@gmail.com>
2017-10-01 12:57:41 -0500

    add test packages

---
+++ server/utils/message.test.js
@@ -0,0 +1,18 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var res = generateMessage({
+      'from': 'Zardoz',
+      'text': 'Kneel, humons'
+    });
+
+    expect(res).toBeA('object');
+    expect(res.from).toBe('Zardoz');
+    expect(res.text).toBe('Kneel, humons');
+    expect(res.createdAt).toBeA('number');
+
+  });
+)};
